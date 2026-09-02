d01cda2b3c00a955676383bbe9f95b57e4db9130
Samson Mano <saminnx@gmail.com>
2026-09-02 00:50:16 -0400

    Add project files.

---
+++ WaveEquation2D_solver/main_frm.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace WaveEquation2D_solver
+{
+    partial class main_frm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main_frm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.solverToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel_zoom_value = new System.Windows.Forms.ToolStripStatusLabel();
+            this.addNodalConstraintsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addEdgeConstraintsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mediumPropertiesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dWaveEquationSolveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importTXTFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importModelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportModelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.loadsToolStripMenuItem,
+            this.solverToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(570, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.importTXTFileToolStripMenuItem,
+            this.importModelToolStripMenuItem,
+            this.exportModelToolStripMenuItem,
+            this.optionToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadsToolStripMenuItem
+            // 
+            this.loadsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addNodalConstraintsToolStripMenuItem,
+            this.addEdgeConstraintsToolStripMenuItem,
+            this.mediumPropertiesToolStripMenuItem});
+            this.loadsToolStripMenuItem.Name = "loadsToolStripMenuItem";
+            this.loadsToolStripMenuItem.Size = new System.Drawing.Size(62, 24);
+            this.loadsToolStripMenuItem.Text = "Loads";
+            // 
+            // solverToolStripMenuItem
+            // 
+            this.solverToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dWaveEquationSolveToolStripMenuItem});
+            this.solverToolStripMenuItem.Name = "solverToolStripMenuItem";
+            this.solverToolStripMenuItem.Size = new System.Drawing.Size(64, 24);
+            this.solverToolStripMenuItem.Text = "Solver";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel_zoom_value});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 278);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(570, 26);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel_zoom_value
+            // 
+            this.toolStripStatusLabel_zoom_value.Name = "toolStripStatusLabel_zoom_value";
+            this.toolStripStatusLabel_zoom_value.Size = new System.Drawing.Size(92, 20);
+            this.toolStripStatusLabel_zoom_value.Text = "Zoom: 100%";
+            // 
+            // addNodalConstraintsToolStripMenuItem
+            // 
+            this.addNodalConstraintsToolStripMenuItem.Name = "addNodalConstraintsToolStripMenuItem";
+            this.addNodalConstraintsToolStripMenuItem.Size = new System.Drawing.Size(242, 26);
+            this.addNodalConstraintsToolStripMenuItem.Text = "Add Nodal Constraints";
+            // 
+            // addEdgeConstraintsToolStripMenuItem
+            // 
+            this.addEdgeConstraintsToolStripMenuItem.Name = "addEdgeConstraintsToolStripMenuItem";
+            this.addEdgeConstraintsToolStripMenuItem.Size = new System.Drawing.Size(242, 26);
+            this.addEdgeConstraintsToolStripMenuItem.Text = "Add Edge Constraints";
+            // 
+            // mediumPropertiesToolStripMenuItem
+            // 
+            this.mediumPropertiesToolStripMenuItem.Name = "mediumPropertiesToolStripMenuItem";
+            this.mediumPropertiesToolStripMenuItem.Size = new System.Drawing.Size(242, 26);
+            this.mediumPropertiesToolStripMenuItem.Text = "Medium Properties";
+            // 
+            // dWaveEquationSolveToolStripMenuItem
+            // 
+            this.dWaveEquationSolveToolStripMenuItem.Name = "dWaveEquationSolveToolStripMenuItem";
+            this.dWaveEquationSolveToolStripMenuItem.Size = new System.Drawing.Size(254, 26);
+            this.dWaveEquationSolveToolStripMenuItem.Text = "2D Wave Equation Solve";
+            // 
+            // importTXTFileToolStripMenuItem
+            // 
+            this.importTXTFileToolStripMenuItem.Name = "importTXTFileToolStripMenuItem";
+            this.importTXTFileToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.importTXTFileToolStripMenuItem.Text = "Import TXT File";
+            // 
+            // importModelToolStripMenuItem
+            // 
+            this.importModelToolStripMenuItem.Name = "importModelToolStripMenuItem";
+            this.importModelToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.importModelToolStripMenuItem.Text = "Import Model";
+            // 
+            // exportModelToolStripMenuItem
+            // 
+            this.exportModelToolStripMenuItem.Name = "exportModelToolStripMenuItem";
+            this.exportModelToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.exportModelToolStripMenuItem.Text = "Export Model";
+            // 
+            // optionToolStripMenuItem
+            // 
+            this.optionToolStripMenuItem.Name = "optionToolStripMenuItem";
+            this.optionToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.optionToolStripMenuItem.Text = "Option";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // main_frm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 304);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "main_frm";
+            this.Text = "2D Wave Equation Solver";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem solverToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel_zoom_value;
+        private System.Windows.Forms.ToolStripMenuItem importTXTFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importModelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportModelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addNodalConstraintsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addEdgeConstraintsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mediumPropertiesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dWaveEquationSolveToolStripMenuItem;
+    }
+}
+
